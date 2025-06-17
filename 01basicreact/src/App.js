8a8f8c7dd5b65c1ce0f9cf4ff4345e34ed74fa86
Abhijit Shete <abhijit.s@amazatic.com>
2025-06-17 11:54:13 +0530

    Deleted old commit.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Run with React | Hello Abhijit!!!</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Run with Vite | Hello Abhijit !!!!!!!</h1>
+  )
+}
+
+export default App
